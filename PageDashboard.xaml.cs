3b3f9f5a985566b94816ca3c9256cd08f2a2495b
Jacky Setiawan <jackysetiawan6@gmail.com>
2023-12-14 18:00:13 +0700

    Add project files.

---
+++ PageDashboard.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Windows.UI.Xaml.Controls;
+using MUXC = Microsoft.UI.Xaml.Controls;
+
+namespace CSharp_Windows_Aid
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# CSharp-WIndows-Aid
