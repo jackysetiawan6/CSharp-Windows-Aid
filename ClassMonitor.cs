2eb4044286483b56aa0b3c3bbbd08251228e2e0e
Jacky Setiawan <jackysetiawan6@gmail.com>
2024-01-18 15:06:12 +0700

    Migrated several code from previous project.

---
+++ ClassMonitor.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics;
+using System.Threading.Tasks;
+using Windows.UI.Popups;
+
+namespace CSharp_Windows_Aid
+{
+    internal class ClassMonitor
+    {
+        private readonly MainPage mainPage;
+
+        public ClassMonitor(MainPage mainPage)
+        {
+            this.mainPage = mainPage;
+        }
+
+        public void Activate()
+        {
+            _ = new MessageDialog("Class Monitor Activated").ShowAsync();
+            _ = Task.Run(() =>
+            {
+                while (true)
+                {
+                    //s
+                }
+            });
+        }
+    }
+}
--- PageDashboard.xaml.cs
+++ PageDashboard.xaml.cs
@@ -1,5 +1,9 @@
-﻿using Windows.UI.Xaml.Controls;
-using MUXC = Microsoft.UI.Xaml.Controls;
+﻿using Windows.ApplicationModel.Core;
+using Windows.UI.ViewManagement;
+using Windows.UI;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml;
+//using MUXC = Microsoft.UI.Xaml.Controls;
 
 namespace CSharp_Windows_Aid
 {
@@ -8,6 +12,29 @@ namespace CSharp_Windows_Aid
         public MainPage()
         {
             this.InitializeComponent();
+            SetWindowTitleBarTransparent(false);
+        }
+
+        private void WindowsAid_Loaded(object _, RoutedEventArgs e)
+        {
+            ClassMonitor classMonitor = new ClassMonitor(this);
+            classMonitor.Activate();
+        }
+
+        private void SetWindowTitleBarTransparent(bool option)
+        {
+            CoreApplicationViewTitleBar coreTitleBar = CoreApplication.GetCurrentView().TitleBar;
+            ApplicationViewTitleBar titleBar = ApplicationView.GetForCurrentView().TitleBar;
+            if (!option)
+            {
+                coreTitleBar.ExtendViewIntoTitleBar = false;
+                titleBar.ButtonBackgroundColor = (Color)Resources["SystemAccentColor"];
+                titleBar.ButtonInactiveBackgroundColor = (Color)Resources["SystemAccentColor"];
+                return;
+            }
+            coreTitleBar.ExtendViewIntoTitleBar = true;
+            titleBar.ButtonBackgroundColor = Colors.Transparent;
+            titleBar.ButtonInactiveBackgroundColor = Colors.Transparent;
         }
     }
 }
